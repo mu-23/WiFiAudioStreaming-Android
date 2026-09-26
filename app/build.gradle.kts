37264a851c4af222f524543176f20a289295cc56
mu-23 <147891807+mu-23@users.noreply.github.com>
2026-09-26 19:12:06 +0800

    Add AppCompat locale support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.activity.ktx)
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
